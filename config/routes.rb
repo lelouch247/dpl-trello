f29dc4ad3084a9c45a99ec8d26a68039471bdb06
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-06-22 10:50:37 -0600

    adding controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Rails.application.routes.draw do
 
   namespace :api do
-    resources :products, except: [:new, :edit]
+    resources :boards do
+      resources :lists
+    end
+
+  resources :lists do
+    resources :cards
   end
+
+  resources :cards do
+    resources :check_lists
+  end
+end
 
   get '*other', to: 'static#index'
 end
